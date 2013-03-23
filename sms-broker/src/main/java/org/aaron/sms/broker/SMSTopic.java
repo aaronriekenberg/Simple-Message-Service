c4567babe3bde81f5eddf126447a59e6f6475f93
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2013-03-23 14:45:03 -0500

    Naming updates.

---
+++ sms-broker/src/main/java/org/aaron/sms/broker/SMSTopic.java
@@ -0,0 +1,54 @@
+package org.aaron.sms.broker;
+
+/*
+ * #%L
+ * Simple Message Service Broker
+ * %%
+ * Copyright (C) 2013 Aaron Riekenberg
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+import org.aaron.sms.protocol.protobuf.SMSProtocol;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.group.ChannelGroup;
+import org.jboss.netty.channel.group.DefaultChannelGroup;
+
+class SMSTopic {
+
+	private final ChannelGroup channelGroup = new DefaultChannelGroup();
+
+	public SMSTopic() {
+
+	}
+
+	public void addSubscription(Channel channel) {
+		channelGroup.add(channel);
+	}
+
+	public void removeSubscription(Channel channel) {
+		channelGroup.remove(channel);
+	}
+
+	public void write(SMSProtocol.BrokerToClientMessage message) {
+		channelGroup.write(ChannelBuffers.wrappedBuffer(message.toByteArray()));
+	}
+}
