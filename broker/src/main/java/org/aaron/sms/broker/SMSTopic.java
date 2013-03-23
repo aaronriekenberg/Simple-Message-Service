a07a32f9637f0993f4dd299ac45526846c0170d0
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2013-03-22 20:49:18 -0500

    Use ChannelGroup instead of DefaultChannelGroup for consistency.

--- broker/src/main/java/org/aaron/sms/broker/SMSTopic.java
+++ broker/src/main/java/org/aaron/sms/broker/SMSTopic.java
@@ -3,11 +3,12 @@ package org.aaron.sms.broker;
 import org.aaron.sms.entities.protobuf.SMSProtocol;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.group.ChannelGroup;
 import org.jboss.netty.channel.group.DefaultChannelGroup;
 
 class SMSTopic {
 
-	private final DefaultChannelGroup channelGroup = new DefaultChannelGroup();
+	private final ChannelGroup channelGroup = new DefaultChannelGroup();
 
 	public SMSTopic() {
 
