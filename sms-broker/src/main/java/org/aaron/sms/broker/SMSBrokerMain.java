12ceb00415be15b0f9b79df0327f2ff394a4a492
aaron <aaron.riekenberg@gmail.com>
2013-04-09 20:40:46 -0500

    Use try with resources for context.

--- sms-broker/src/main/java/org/aaron/sms/broker/SMSBrokerMain.java
+++ sms-broker/src/main/java/org/aaron/sms/broker/SMSBrokerMain.java
@@ -31,15 +31,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SMSBrokerMain {
 
 	public static void main(String[] args) {
-		final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				"META-INF/spring/sms-broker-spring.xml");
-		final SMSBrokerTCPServer brokerTCPServer = context
-				.getBean(SMSBrokerTCPServer.class);
-		while (true) {
-			try {
-				brokerTCPServer.awaitBrokerDestroyed();
-				break;
-			} catch (InterruptedException e) {
+		try (final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"META-INF/spring/sms-broker-spring.xml")) {
+			final SMSBrokerTCPServer brokerTCPServer = context
+					.getBean(SMSBrokerTCPServer.class);
+			while (true) {
+				try {
+					brokerTCPServer.awaitBrokerDestroyed();
+					break;
+				} catch (InterruptedException e) {
+				}
 			}
 		}
 	}
