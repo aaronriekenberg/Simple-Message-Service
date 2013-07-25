e6592e7930a23929930ee78ca2e53c323eba24c0
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2013-07-24 21:58:26 -0500

    Add null checks.

--- sms-broker/src/main/java/org/aaron/sms/broker/SMSTopic.java
+++ sms-broker/src/main/java/org/aaron/sms/broker/SMSTopic.java
@@ -26,6 +26,7 @@ package org.aaron.sms.broker;
  * #L%
  */
 
+import static com.google.common.base.Preconditions.checkNotNull;
 import io.netty.channel.Channel;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
@@ -43,14 +44,20 @@ class SMSTopic {
 	}
 
 	public void addSubscription(Channel channel) {
+		checkNotNull(channel, "channel is null");
+
 		channelGroup.add(channel);
 	}
 
 	public void removeSubscription(Channel channel) {
+		checkNotNull(channel, "channel is null");
+
 		channelGroup.remove(channel);
 	}
 
 	public void write(SMSProtocol.BrokerToClientMessage message) {
+		checkNotNull(message, "message is null");
+
 		channelGroup.flushAndWrite(message);
 	}
 }
