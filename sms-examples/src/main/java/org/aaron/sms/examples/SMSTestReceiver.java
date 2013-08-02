367c39d34404b6d97f94148c5ced35249effa7f4
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2013-08-02 14:09:43 -0500

    Netty 4.0.6.Final.

--- sms-examples/src/main/java/org/aaron/sms/examples/SMSTestReceiver.java
+++ sms-examples/src/main/java/org/aaron/sms/examples/SMSTestReceiver.java
@@ -97,7 +97,7 @@ public class SMSTestReceiver {
 	}
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 30; ++i) {
+		for (int i = 0; i < 50; ++i) {
 			new SMSTestReceiver("test.topic." + i).start();
 		}
 		while (true) {
