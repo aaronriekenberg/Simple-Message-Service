83f296270724fa688b89902763af7ba928944dfd
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2015-05-23 05:47:28 -0500

    Reduce duplication.

--- sms-common/src/main/java/org/aaron/sms/common/util/FunctionalReentrantReadWriteLock.java
+++ sms-common/src/main/java/org/aaron/sms/common/util/FunctionalReentrantReadWriteLock.java
@@ -28,32 +28,40 @@ package org.aaron.sms.common.util;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class FunctionalReentrantReadWriteLock extends ReentrantReadWriteLock {
 
 	private static final long serialVersionUID = 1L;
 
-	public void doInReadLock(Runnable r) {
-		checkNotNull(r, "r is null");
+	public FunctionalReentrantReadWriteLock() {
+		super();
+	}
 
-		readLock().lock();
+	public FunctionalReentrantReadWriteLock(boolean fair) {
+		super(fair);
+	}
+
+	private void doInLock(Lock lock, Runnable r) {
+		lock.lock();
 		try {
 			r.run();
 		} finally {
-			readLock().unlock();
+			lock.unlock();
 		}
+	}
+
+	public void doInReadLock(Runnable r) {
+		checkNotNull(r, "r is null");
+
+		doInLock(readLock(), r);
 	}
 
 	public void doInWriteLock(Runnable r) {
 		checkNotNull(r, "r is null");
 
-		writeLock().lock();
-		try {
-			r.run();
-		} finally {
-			writeLock().unlock();
-		}
+		doInLock(writeLock(), r);
 	}
 
 }
